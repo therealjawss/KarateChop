8d0f9c77a4b41e2f47d18aa69a2a18f9d20e6324
therealjawss <jawss@thecodinghammock.com>
2019-07-25 12:28:08 +0200

    Initial Commit

---
+++ KarateChop/KarateChop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KarateChop
+{
+    public class KarateChop
+    {
+        public static int Chop(int number, int[] array_of_int)
+        {
+            
+        }
+    }
+}
